f44e76b9482dbba38004b34761f5b2e88d3e398e
Aliyeh Ebrahimi <ebrahimialiyeh@gmail.com>
2025-03-06 20:32:34 -0500

    add main file

---
+++ main.py
@@ -0,0 +1,14 @@
+# Main
+# YOUR NAME
+# The start of your overall agent program.
+# Demonstrate the use of your SimpleReflexVacuum and your ModelReflexVacuum.
+# The code for using a SimpleReflexVacuum is provided for you.
+
+
+# Part 1
+from simple_reflex_vacuum import main as simple_main
+simple_main()
+
+# Part 2
+from model_reflex_vacuum import main as model_main
+model_main()
